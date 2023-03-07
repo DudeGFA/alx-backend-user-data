b6568abf01b2ac41dd94804f41651ea4186e9687
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-07 16:55:51 +0100

    Created BasicAuth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    Conatins class BasicAuth
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+        Implements Basic authentication
+    """
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+            returns the Base64 part
+            of the Authorization header
+            for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
