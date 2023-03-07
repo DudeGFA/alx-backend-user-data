7a1ba33a1360fd8a04ab968f89be5e642f961f5e
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-07 16:35:09 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -45,7 +45,7 @@ def before_req():
         filters each request
     """
     if auth is not None:
-        if auth.require_auth(request.path,
+        if not auth.require_auth(request.path,
                              ['/api/v1/status/',
                               '/api/v1/unauthorized/',
                               '/api/v1/forbidden/']):
