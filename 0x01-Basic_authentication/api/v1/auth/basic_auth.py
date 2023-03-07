67acb58e8ad66d2cebaad2cbe6ffac9a1de3fa16
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-07 17:04:41 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
     Conatins class BasicAuth
 """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -24,3 +25,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+            returns the decoded value
+            of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            auth_header = base64.b64decode(base64_authorization_header)
+        except Exception:
+            return None
+        return auth_header.decode('utf-8')
