98d07bd0fff3dc887c75b510d08451e08ca7db08
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-09 00:12:29 +0100

    Update users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -29,6 +29,9 @@ def view_one_user(user_id: str = None) -> str:
         abort(404)
     if user_id == 'me' and request.current_user is None:
         abort(404)
+    if user_id == 'me' and request.current_user is not None:
+        all_users = [user.to_json() for user in User.all()]
+        return jsonify(all_users[0])
     user = User.get(user_id)
     if user is None:
         abort(404)
