77b5358fb3fa609ddc2f63d76251cb1a0b16e6d0
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-09 01:34:56 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 """
 from .auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -30,3 +31,14 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+            returns a User instance
+            based on a cookie value
+            Args:
+                request: request object
+        """
+        session_cookie = self.session_cookie(request)
+        userID = self.user_id_by_session_id.get(session_cookie)
+        return User.get(userID)
