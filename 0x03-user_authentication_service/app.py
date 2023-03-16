845c27fbdd139deaa244c7602df7fd557ecd61eb
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-16 15:36:06 +0100

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -92,8 +92,11 @@ def get_reset_password_token() -> str:
     email = request.form.get("email")
     if email is None:
         abort(403)
-    token = AUTH.get_reset_password_token(email)
-    return jsonify({"email": email, "reset_token": token}), 200
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
