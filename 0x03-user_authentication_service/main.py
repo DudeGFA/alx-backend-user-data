661e6546fd3d78c762059e2805fff3f226504d37
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-16 16:23:18 +0100

    Update main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
+"""
+    Contains test functions for
+    app.py
+"""
 import requests
 
+
 def register_user(email: str, password: str) -> None:
-    resp = requests.post('localhost:500/users', data = {'email': email, 'password': password})
+    """
+        tests POST /users endpoint
+    """
+    resp = requests.post(
+        'localhost:500/users',
+        data={
+            'email': email,
+            'password': password})
     assert resp == {"message": "Bienvenue"}
+
 
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
