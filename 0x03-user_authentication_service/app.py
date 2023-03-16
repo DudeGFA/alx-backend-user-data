52ffe2a13dd7927ca9150afaef4f65e190ccbf6e
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-03-16 15:06:42 +0100

    Update app.py
    implement a profile function to respond to the GET /profile route.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,5 +67,19 @@ def logout() -> str:
     abort(403)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """
+        returns a user's details
+    """
+    sessionID = request.cookies.get('session_id')
+    if sessionID is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(sessionID)
+    if user is not None:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
